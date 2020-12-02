7284a6e76d9d4dd5c927731083e43ce884c776ca
Thalys Viana <thalysvianaguiar@gmail.com>
2020-12-02 05:40:30 -0300

    Update example code

--- Example/TVMaskField/ViewController.swift
+++ Example/TVMaskField/ViewController.swift
@@ -17,6 +17,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        maskTextField.delegate = self
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let oldText = textField.text as NSString? else { return true }
+        let newText = oldText.replacingCharacters(in: range, with: string)
+        
+        if textField == maskTextField {
+            resultsTextField.textMask = newText
+        }
+        return true
     }
 }
 
